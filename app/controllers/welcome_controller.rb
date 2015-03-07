c450adad9a39f6ab81143abadee1a159469e14f7
Igor Shcherbak <igorski_is@yahoo.com>
2015-03-07 16:20:24 -0700

    Completed my_first_rails_app
    [#89855900]

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def hello
   end
+
+  def time
+  end
+
+  def introduce
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'hello' => 'welcome#hello'
   get 'hello/:name' => 'welcome#hello'
+  get 'time/now' => 'welcome#time'
+  get 'introduce/:name1/and/:name2' => 'welcome#introduce'
 end
